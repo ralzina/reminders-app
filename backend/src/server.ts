1725a6733b5c252d630b7c08cf489036f9b474c5
Rene Alzina <renealzina@Renes-MacBook-Pro.local>
2026-08-03 09:36:51 -0700

    Added cors options

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -43,12 +43,10 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200 
-  };
+};
   
-  app.use(cors(corsOptions));
-  
-  app.options('*', cors(corsOptions));
-
+app.use(cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
